d93262660afc0cb5a315c9e488f3429481677d0a
harshitScript <85029089+harshitScript@users.noreply.github.com>
2022-06-30 22:00:10 +0530

    full product storing and retrival logic added

--- Controllers/admin/products/postProductsController.js
+++ Controllers/admin/products/postProductsController.js
@@ -1,13 +1,12 @@
 const Product = require("../../../Modals/product");
 
-const postProductsController = (req, res, next) => {
-  if (req.method === "POST") {
-    const product = new Product(req?.body?.title);
+const postProductsController = (req, res) => {
+  const { title, price, description, imageUrl } = req.body;
+  const product = new Product(title, imageUrl, price, description);
 
-    product.save();
-
+  product.save(() => {
     return res.redirect("/shop/");
-  }
+  });
 };
 
 module.exports = { middleWare: postProductsController };
--- Modals/product.js
+++ Modals/product.js
@@ -15,18 +15,22 @@ const getProductsFromFile = (callback = () => {}) => {
 module.exports = class Product {
   //? "this" keyword refer to the object to be returned by the class.
 
-  constructor(t) {
-    this.title = t;
+  constructor( title, imageUrl, price, description ) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
   }
 
   //? will be called on the instance object.
-  save() {
+  save(callback = () => {}) {
     getProductsFromFile((products) => {
       //! "this" should always be kept within class.
       products.push(this);
 
       fs.writeFile(p, JSON.stringify(products), (err) => {
         if (err) return console.log("Error while saving data.");
+        callback();
       });
     });
   }
