2c2ffe53be73554ad3b8784770ca813c79681139
harshitScript <85029089+harshitScript@users.noreply.github.com>
2022-08-31 17:20:38 +0530

    email functionality added

--- Controllers/auth/postLoginController.js
+++ Controllers/auth/postLoginController.js
@@ -1,5 +1,16 @@
 const User = require("../../Modals/User");
 const { generateHashedPassword } = require("../../util/helper");
+const { createTransport } = require("nodemailer");
+const sendGridTransport = require("nodemailer-sendgrid-transport");
+
+const transport = createTransport(
+  sendGridTransport({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
 
 const postLoginController = (req, res) => {
   const { email, password } = req.body;
@@ -11,14 +22,21 @@ const postLoginController = (req, res) => {
     .then((user) => {
       if (user) {
         req.session.userId = user?._id;
-        return req.session.save((err) => {
-          if (err)
-            return console.log("error occurred while saving the session.");
-          return res.redirect("/shop/");
-        });
+        return req.session.save();
       }
       req.flash("error", "User not found.");
-      return res.redirect("/auth/login");
+      res.redirect("/auth/login");
+    })
+    .then(() => {
+      return transport.sendMail({
+        to: email,
+        from: process.env.SERVER_MAIL,
+        subject: "Login successful.",
+        html: "<p>welcome to your own express shop 2022.</p>",
+      });
+    })
+    .then(() => {
+      return res.redirect("/shop/");
     })
     .catch((error) => {
       console.log("The error is : ", error);
